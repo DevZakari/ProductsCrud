e8468e8746c4850cf8abf57e05b0fe429d1483a4
DevZakari <75742235+DevZakari@users.noreply.github.com>
2023-10-17 01:45:54 +0100

    Ajoutez des fichiers projet.

---
+++ CrudProducts/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CrudProducts.Data;
+using CrudProducts.Model;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace CrudProducts.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CrudProducts.Data.CrudProductsContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public CreateModel(CrudProducts.Data.CrudProductsContext context, IWebHostEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+        // Property to represent the uploaded image file
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+
+        [HttpPost]
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                // Generate a unique filename using a timestamp
+                var fileName = DateTime.Now.Ticks + Path.GetExtension(ImageFile.FileName);
+
+                var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                // Ensure the uploads folder exists
+                Directory.CreateDirectory(uploadsFolder);
+
+                // Save the file to the server
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(fileStream);
+                }
+
+                // Save the file path in your database
+                Product.imageUrl = "/uploads/" + fileName;
+                // Update the path as per your project structure
+
+            }
+            _context.Product.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CrudProducts/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CrudProducts.Data;
+using CrudProducts.Model;
+using System.Text.Json;
+
+
+namespace CrudProducts.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CrudProducts.Data.CrudProductsContext _context;
+
+        public IndexModel(CrudProducts.Data.CrudProductsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string prd { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public decimal? minPrice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? maxPrice { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<Product> productsQuery = _context.Product;
+
+            if (!string.IsNullOrEmpty(prd))
+            {
+                // Filter based on the search term
+                productsQuery = productsQuery
+                    .Where(p => p.Name.Contains(prd));
+            }
+            if (minPrice.HasValue)
+            {
+                productsQuery = productsQuery.Where(p => p.Price >= minPrice.Value);
+            }
+
+            if (maxPrice.HasValue)
+            {
+                productsQuery = productsQuery.Where(p => p.Price <= maxPrice.Value);
+            }
+
+            // Affectez les résultats filtrés à la propriété Product
+            Product = await productsQuery.ToListAsync();
+        }
+    }
+}
+
